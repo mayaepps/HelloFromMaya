e5cf4f87662e7f89e3f6022014e54080c3e80bd6
mayaepps <mepps@lion.lmu.edu>
2020-06-04 13:54:11 -0700

    required features

---
+++ app/src/main/java/com/example/hellomaya/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hellomaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary) );
+            }
+        });
+    }
+}
