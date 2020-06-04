370878921c673ec0b3aa6d4fa285b504710aa133
mayaepps <mepps@lion.lmu.edu>
2020-06-04 15:32:25 -0700

    optional user stories

--- app/src/main/java/com/example/hellomaya/MainActivity.java
+++ app/src/main/java/com/example/hellomaya/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.hellomaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +22,50 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getColor(R.color.colorPrimary) );
             }
         });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.orange));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //grab the text input
+                String newText = ((EditText) findViewById(R.id.editText)).getText().toString();
+
+                //put the text into into the text view once the 'change text' button is clicked
+                if (TextUtils.isEmpty(newText)) {
+                    ((TextView) findViewById(R.id.textView)).setText("Goodbye! :)");
+                } else {
+                    ((TextView) findViewById(R.id.textView)).setText(newText);
+                }
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //reset text color back to black
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.black));
+
+                //reset background color
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.colorAccent));
+
+                //reset text back to hello from maya
+                ((TextView) findViewById(R.id.textView)).setText(R.string.hello_from_maya);
+
+                //reset the editText
+                ((EditText) findViewById(R.id.editText)).setText("");
+
+            }
+        });
+
     }
 }
